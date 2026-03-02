e3b4ea134b6a33e1d24be2f238ba7c1ebb775870
rumbahuh <r.castilla.2023@alumnos.urjc.es>
2026-03-02 11:15:56 +0100

    Done

--- 2025-2026/PRACTICAS/2/myfgrep.c
+++ 2025-2026/PRACTICAS/2/myfgrep.c
@@ -7,25 +7,31 @@ myfgrep(int arguments, char * chain[])
 {
 	int numberofwordstosearch = arguments - 1;
 	int status = 0;
-
-	int argsofchain = arguments + 1 + 1; // terminated in NULL plus [0] is function name
-	char ** allocatedargs = malloc(sizeof(char*) * argsofchain); // allocated memory for array of strings
+	int argsofchain = arguments + 1 + 1 + 1 + numberofwordstosearch; // terminated in NULL + [0] is function name  + each word has -e
 	int i = 0;
+	int j = 2;
+
+	char ** allocatedargs = malloc(sizeof(char*) * argsofchain); // allocated memory for array of strings
+	if (allocatedargs == NULL) return 1;
 
 	allocatedargs[0] = "fgrep";
-	for (; i < arguments; i++) {
-		allocatedargs[i + 1] = chain[i];
+	allocatedargs[1] = "-n";
+	for (; i < numberofwordstosearch; i++) {
+		allocatedargs[j++] = "-e";
+		allocatedargs[j++] = chain[i];
 	}
-	allocatedargs[argsofchain - 1] = NULL; // last index is total -1
-
-	(void)numberofwordstosearch;
+	allocatedargs[j++] = chain[numberofwordstosearch]; // filename
+	allocatedargs[j] = NULL; // last index is total -1
+	
 	switch (fork()) {
 	case -1:
 		status = 1;
 		break;
 	case 0:
-		execv("/bin/fgrep", allocatedargs); // no está buscando las que yo quiero solo la primera como pattern
+		execv("/bin/fgrep", allocatedargs);
 		status = 1;
+		free(allocatedargs);
+		exit(1);
 		break;
 	}
 
@@ -47,7 +53,6 @@ main(int argc, char * argv[])
 
 	statusmain = myfgrep(argc, argv);
 	if (statusmain) fprintf(stderr, "file grep failure\n");
-	printf("am i alive?\n\n");
 
 	return statusmain;
 }
