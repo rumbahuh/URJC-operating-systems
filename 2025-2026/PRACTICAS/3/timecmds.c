9d17972ed0f20c930bd886f70ebc843309a8267c
rumbahuh <r.castilla.2023@alumnos.urjc.es>
2026-03-06 18:58:43 +0100

    Los pointers en C van a ser el fin de mi existencia, que horror, que terrible, que amargura, que verguenza y no llevo ni la mitad del ejercicio pero al menos ya lo que más me cuesta me lo he quitado de encima

--- 2025-2026/PRACTICAS/3/timecmds.c
+++ 2025-2026/PRACTICAS/3/timecmds.c
@@ -2,29 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
+enum {
+    MAX_TOKENS_PER_ARG = 64
+};
+
 int
-tokenize(char * argument)
+tokenize(char *argument, char **buffer)
 {
-	int localstatus = 0, i = 0;;
+	int localstatus = 0, i = 0;
 
 	// from strtok_r(3) man page
 	char *str1, *str2, *token, *subtoken;
     char *saveptr1, *saveptr2;
 	const char *restrict delim = " ";
 
-    for (str1 = argument; ; i++, str1 = NULL) {
+    for (str1 = argument; ; str1 = NULL) {
         token = strtok_r(str1, delim, &saveptr1);
         if (token == NULL)
             break;
-        printf("%d: %s\n", i, token);
 
         for (str2 = token; ; str2 = NULL) {
             subtoken = strtok_r(str2, delim, &saveptr2);
             if (subtoken == NULL)
                 break;
-            printf("\t --> %s\n", subtoken);
+			if (i < MAX_TOKENS_PER_ARG - 1) {
+                buffer[i] = subtoken;
+                i++;
             }
         }
+    }
+	buffer[i] = NULL;
 
 	return localstatus;
 }
@@ -32,18 +39,34 @@ tokenize(char * argument)
 int
 main(int argc, char * argv[])
 {
-	int status = 0, i = 0, j = 0;
+	int status = 0, i = 0, j = 0, k = 0;
 	argc--;
 	argv++;
 
+	// array of arrays of strings
+	char ***argvptrs = malloc(sizeof(char **) * argc);
+	char **copyofargv = malloc(sizeof(char *) * argc);
+	if (copyofargv == NULL || argvptrs == NULL) return 1;
 	for(; i < argc; i++) {
-		status = tokenize(argv[i]);
+		argvptrs[i] = malloc(sizeof(char *) * MAX_TOKENS_PER_ARG);
+		copyofargv[i] = malloc(strlen(argv[i]) + 1);
+		strcpy(copyofargv[i], argv[i]);
+		tokenize(copyofargv[i], argvptrs[i]);
 	}
 	
-	// We check new argv structure
-	for(; j < argc; j++) {
-		fprintf(stdout, "%s\n", argv[j]);
+	for (; k < argc; k++) {
+        printf("Shelf %d content:\n", i);
+        for (int l = 0; argvptrs[k][l] != NULL; l++) {
+            printf("  Slot %d: %s\n", l, argvptrs[k][l]);
+        }
+    }
+
+	for (; j < argc; j++) {
+		free(argvptrs[j]);
+		free(copyofargv[j]);
 	}
-	
+	free(argvptrs);
+	free(copyofargv);
+
 	return status;
 }
