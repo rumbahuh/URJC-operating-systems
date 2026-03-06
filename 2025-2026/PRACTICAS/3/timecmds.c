7e1b4cdd7e0ccefc51a20faa1b111f7fefa8b59f
rumbahuh <r.castilla.2023@alumnos.urjc.es>
2026-03-06 18:02:16 +0100

    tokenize functioning print, to keep track of storage still to implement

--- 2025-2026/PRACTICAS/3/timecmds.c
+++ 2025-2026/PRACTICAS/3/timecmds.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int
-tokenize(int *num, char * chain[])
+tokenize(char * argument)
 {
-	int localstatus = 0;
-	(void)chain;
-	*num = 2;
+	int localstatus = 0, i = 0;;
+
+	// from strtok_r(3) man page
+	char *str1, *str2, *token, *subtoken;
+    char *saveptr1, *saveptr2;
+	const char *restrict delim = " ";
+
+    for (str1 = argument; ; i++, str1 = NULL) {
+        token = strtok_r(str1, delim, &saveptr1);
+        if (token == NULL)
+            break;
+        printf("%d: %s\n", i, token);
+
+        for (str2 = token; ; str2 = NULL) {
+            subtoken = strtok_r(str2, delim, &saveptr2);
+            if (subtoken == NULL)
+                break;
+            printf("\t --> %s\n", subtoken);
+            }
+        }
 
 	return localstatus;
 }
@@ -13,15 +32,17 @@ tokenize(int *num, char * chain[])
 int
 main(int argc, char * argv[])
 {
-	int status = 0, i = 0;
+	int status = 0, i = 0, j = 0;
 	argc--;
 	argv++;
 
-	status = tokenize(&argc, argv);
+	for(; i < argc; i++) {
+		status = tokenize(argv[i]);
+	}
 	
 	// We check new argv structure
-	for(; i < argc; i++) {
-		fprintf(stdout, "%s\n", argv[i]);
+	for(; j < argc; j++) {
+		fprintf(stdout, "%s\n", argv[j]);
 	}
 	
 	return status;
