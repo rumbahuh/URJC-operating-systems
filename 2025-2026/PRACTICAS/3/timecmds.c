0bffe333730db7ccfc49229de6d7a7af94f4a38f
rumbahuh <r.castilla.2023@alumnos.urjc.es>
2026-03-07 08:58:19 +0100

    Done and indent.sh

--- 2025-2026/PRACTICAS/3/timecmds.c
+++ 2025-2026/PRACTICAS/3/timecmds.c
@@ -3,16 +3,18 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <time.h>
 
 enum {
-    MAX_TOKENS_PER_ARG = 64
+	MAX_TOKENS_PER_ARG = 64
 };
 
 struct timecmd {
-	char * cmd;
+	char *fullstring;
+	// char *cmd;
 	pid_t num;
-	int time;
-	char * status;
+	time_t start;
+	char *status;
 };
 
 int
@@ -22,99 +24,143 @@ tokenize(char *argument, char **buffer)
 
 	// from strtok_r(3) man page
 	char *str1, *str2, *token, *subtoken;
-    char *saveptr1, *saveptr2;
+	char *saveptr1, *saveptr2;
 	const char *restrict delim = " ";
 
-    for (str1 = argument; ; str1 = NULL) {
-        token = strtok_r(str1, delim, &saveptr1);
-        if (token == NULL)
-            break;
+	for (str1 = argument;; str1 = NULL) {
+		token = strtok_r(str1, delim, &saveptr1);
+		if (token == NULL)
+			break;
 
-        for (str2 = token; ; str2 = NULL) {
-            subtoken = strtok_r(str2, delim, &saveptr2);
-            if (subtoken == NULL)
-                break;
+		for (str2 = token;; str2 = NULL) {
+			subtoken = strtok_r(str2, delim, &saveptr2);
+			if (subtoken == NULL)
+				break;
 			if (i < MAX_TOKENS_PER_ARG - 1) {
-                buffer[i] = subtoken;
-                i++;
-            }
-        }
-    }
+				buffer[i] = subtoken;
+				i++;
+			}
+		}
+	}
 	buffer[i] = NULL;
 
 	return status;
 }
 
 int
-executeprogram(char ***argvpointers, int position, struct timecmd * chain)
+executeprogram(char ***argvpointers, int position, struct timecmd *chain)
 {
-	int status = 0;
 	pid_t pid = 0;
 
 	char *path = argvpointers[position][0];
+
 	// already terminated in NULL in definition
 	char **args = argvpointers[position];
 
+	chain[position].start = time(NULL);
 	pid = fork();
 	if (pid > 0) {
 		chain[position].num = pid;
-		chain[position].cmd = path;
+		// chain[position].cmd = path;
 	}
 
-	switch(pid) {
+	switch (pid) {
 	case -1:
 		fprintf(stderr, "Fork failed\n");
 		return -1;
 	case 0:
 		execv(path, args);
-		fprintf(stderr, "Execution failed\n");
-		status = -1;
-		break;
+		exit(1);
+	}
+
+	return 0;
+}
+
+void
+forkall(char ***argvptrs, struct timecmd *timecmds, char **argv, int argc)
+{
+	for (int k = 0; k < argc; k++) {
+		timecmds[k].fullstring = argv[k];
+		executeprogram(argvptrs, k, timecmds);
+	}
+}
+
+int
+waitall(struct timecmd *timecmds, int argc)
+{
+	int status = 0;
+
+	for (int j = 0; j < argc; j++) {
+		int wstatus = 0;
+		pid_t finished_pid = wait(&wstatus);
+		time_t end = time(NULL);
+
+		struct timecmd *cmd = NULL;
+
+		for (int m = 0; m < argc; m++) {
+			if (timecmds[m].num == finished_pid) {
+				cmd = &timecmds[m];
+				break;
+			}
+		}
+		if (cmd == NULL)
+			continue;
+
+		int elapsed = (int)(end - cmd->start);
+
+		if (WIFEXITED(wstatus) && WEXITSTATUS(wstatus) == 0) {
+			cmd->status = "success";
+		} else {
+			cmd->status = "failure";
+			status = -1;
+		}
+		printf("cmd: %s, pid: %d, time: %d seconds, status: %s\n",
+		       cmd->fullstring, finished_pid, elapsed, cmd->status);
 	}
 
 	return status;
 }
 
-int
-main(int argc, char * argv[])
+void
+freeall(char ***argvptrs, char **copyofargv, int argc, struct timecmd *timecmds)
 {
-	int status = 0, i = 0, j = 0, k = 0, l = 0;
-	argc--;
-	argv++;
-
-	struct timecmd *timecmds = malloc(sizeof(struct timecmd) * argc);
-	// array of arrays of strings
-	char ***argvptrs = malloc(sizeof(char **) * argc);
-	char **copyofargv = malloc(sizeof(char *) * argc);
-	if (copyofargv == NULL || argvptrs == NULL) return 1;
-	for(; i < argc; i++) {
-		argvptrs[i] = malloc(sizeof(char *) * MAX_TOKENS_PER_ARG);
-		copyofargv[i] = malloc(strlen(argv[i]) + 1);
-		strcpy(copyofargv[i], argv[i]);
-		tokenize(copyofargv[i], argvptrs[i]);
-	}
-	
-	for (; k < argc; k++) {
-		status = executeprogram(argvptrs, k, timecmds);
-		if (status) {
-			timecmds[k].status = "success";
-		} else {
-			timecmds[k].status = "failure";
-		}
-    }
-
-	for(; j < argc; j++) {
-		pid_t finished_pid = waitpid(timecmds[j].num, &status, 0);
-		printf("cmd: %s, pid: %d, status: %s\n", timecmds[j].cmd, finished_pid, timecmds[j].status);
-	} 
-
-	for (; l < argc; l++) {
+	for (int l = 0; l < argc; l++) {
 		free(argvptrs[l]);
 		free(copyofargv[l]);
 	}
 	free(argvptrs);
 	free(copyofargv);
 	free(timecmds);
+}
+
+int
+main(int argc, char *argv[])
+{
+	int status = 0, i = 0;
+
+	argc--;
+	argv++;
+
+	struct timecmd *timecmds = malloc(sizeof(struct timecmd) * argc);
+
+	// array of arrays of strings
+	char ***argvptrs = malloc(sizeof(char **) * argc);
+	char **copyofargv = malloc(sizeof(char *) * argc);
+
+	if (copyofargv == NULL || argvptrs == NULL || timecmds == NULL)
+		return 1;
+
+	for (; i < argc; i++) {
+		argvptrs[i] = malloc(sizeof(char *) * MAX_TOKENS_PER_ARG);
+		copyofargv[i] = malloc(strlen(argv[i]) + 1);
+		strcpy(copyofargv[i], argv[i]);
+		tokenize(copyofargv[i], argvptrs[i]);
+	}
+
+	forkall(argvptrs, timecmds, argv, argc);
+	status = waitall(timecmds, argc);
+
+	freeall(argvptrs, copyofargv, argc, timecmds);
 
 	return status;
 }
