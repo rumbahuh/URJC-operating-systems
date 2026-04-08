1844cd97e2643c0b14811cc9e31fb533c7f55e62
rumbahuh <r.castilla.2023@alumnos.urjc.es>
2026-04-08 11:37:04 +0200

    indent.sh run

--- 2025-2026/PRACTICAS/5/mysplit.c
+++ 2025-2026/PRACTICAS/5/mysplit.c
@@ -11,7 +11,7 @@
  * Handles errors.
  */
 int
-parsestring(char * stringnumber)
+parsestring(char *stringnumber)
 {
 	int base = 10;
 	char *endptr, *str;
@@ -22,14 +22,14 @@ parsestring(char * stringnumber)
 	val = strtol(str, &endptr, base);
 
 	if (errno == ERANGE) {
-        perror("strtol");
-        return -1;
-    }
+		perror("strtol");
+		return -1;
+	}
 
-    if (endptr == str) {
-        fprintf(stderr, "No digits were found\n");
-        return -1;
-    }
+	if (endptr == str) {
+		fprintf(stderr, "No digits were found\n");
+		return -1;
+	}
 
 	return val;
 }
@@ -41,26 +41,26 @@ parsestring(char * stringnumber)
  * byte position of total of file bytes.
  */
 int
-calculatesubfiles(int nbytes, char * file, char * buffer)
+calculatesubfiles(int nbytes, char *file, char *buffer)
 {
 	int nr;
 	int nsubfiles = 0;
 	int fd;
 
 	fd = open(file, O_RDONLY);
-    if (fd < 0) {
-        fprintf(stderr, "can't open %s\n", file);
-        return -1;
-    }
+	if (fd < 0) {
+		fprintf(stderr, "can't open %s\n", file);
+		return -1;
+	}
 
-	while ((nr = read(fd, buffer, nbytes)) > 0) 
+	while ((nr = read(fd, buffer, nbytes)) > 0)
 		nsubfiles++;
 	if (nr < 0) {
 		fprintf(stderr, "can't read\n");
 		close(fd);
-    	return -1;
+		return -1;
 	}
-	
+
 	close(fd);
 	return nsubfiles;
 }
@@ -71,12 +71,12 @@ calculatesubfiles(int nbytes, char * file, char * buffer)
  * Adds it to buffernames.
  */
 void
-createnames(char ** buffernames, int filenumbers, int length, char * filename)
+createnames(char **buffernames, int filenumbers, int length, char *filename)
 {
 	int i = 0;
 
-	for(; i < filenumbers; i++) {
-        snprintf(buffernames[i], length, "%03d%s", i, filename);
+	for (; i < filenumbers; i++) {
+		snprintf(buffernames[i], length, "%03d%s", i, filename);
 	}
 }
 
@@ -84,74 +84,75 @@ createnames(char ** buffernames, int filenumbers, int length, char * filename)
  * Creates file and write bytes from file up to Nbytes.
  */
 int
-splitfile(char ** buffernames, int filenumbers, int nbytes, char * file, char * buffer)
+splitfile(char **buffernames, int filenumbers, int nbytes, char *file,
+	  char *buffer)
 {
 	int i = 0;
 	int fd_in, fd_out;
 	int nr, nw, written;
 
 	fd_in = open(file, O_RDONLY);
-    if (fd_in < 0) {
-        fprintf(stderr, "can't open %s\n", file);
-        return -1;
-    }
+	if (fd_in < 0) {
+		fprintf(stderr, "can't open %s\n", file);
+		return -1;
+	}
 
-	for(; i < filenumbers; i++) {
-		fd_out = open(buffernames[i], O_WRONLY | O_CREAT | O_TRUNC, 0664); // TRUNC is an spec
+	for (; i < filenumbers; i++) {
+		fd_out = open(buffernames[i], O_WRONLY | O_CREAT | O_TRUNC, 0664);	// TRUNC is an spec
 
 		if (fd_out < 0) {
-            fprintf(stderr, "can't open %s\n", buffernames[i]);
-            close(fd_in);
-            return -1;
-        }
-        nr = read(fd_in, buffer, nbytes);
-        if (nr < 0) {
-            fprintf(stderr, "can't read\n");
-            close(fd_out);
-            close(fd_in);
-            return -1;
-        }
+			fprintf(stderr, "can't open %s\n", buffernames[i]);
+			close(fd_in);
+			return -1;
+		}
+		nr = read(fd_in, buffer, nbytes);
+		if (nr < 0) {
+			fprintf(stderr, "can't read\n");
+			close(fd_out);
+			close(fd_in);
+			return -1;
+		}
 
 		written = 0;
 
 		while (written < nr) {
-            nw = write(fd_out, buffer + written, nr - written);
-            if (nw < 0) {
-                fprintf(stderr, "can't write\n");
-                close(fd_out);
-                close(fd_in);
-                return -1;
-            }
-            written += nw;
-        }
-        close(fd_out);
-    }
-    close(fd_in);
+			nw = write(fd_out, buffer + written, nr - written);
+			if (nw < 0) {
+				fprintf(stderr, "can't write\n");
+				close(fd_out);
+				close(fd_in);
+				return -1;
+			}
+			written += nw;
+		}
+		close(fd_out);
+	}
+	close(fd_in);
 	return 0;
 }
 
 void
-freeall(char * parent, char ** files, int number)
+freeall(char *parent, char **files, int number)
 {
 	int i = 0;
 
 	if (files != NULL) {
-        for (; i < number; i++) {
-            free(files[i]);
-        }
-        free(files);
-    }
-    free(parent);
+		for (; i < number; i++) {
+			free(files[i]);
+		}
+		free(files);
+	}
+	free(parent);
 }
 
 int
-main(int argc, char* argv[])
+main(int argc, char *argv[])
 {
-	char * parentfile;
+	char *parentfile;
 	int nbytes = 0;
 	int subfiles = 0;
-	char ** filenames;
-	char * parentbuffer;
+	char **filenames;
+	char *parentbuffer;
 	int status = 0;
 	int len = 0;
 	int j = 0;
@@ -164,15 +165,16 @@ main(int argc, char* argv[])
 	}
 
 	parentfile = argv[1];
-	len = strlen("000") + strlen(parentfile) + 1; // + '\0'
+	len = strlen("000") + strlen(parentfile) + 1;	// + '\0'
 
 	nbytes = parsestring(argv[0]);
 	if (nbytes == -1) {
 		fprintf(stderr, "usage: mysplit N file\n");
 		exit(EXIT_FAILURE);
 	}
-	if (nbytes <= 0) exit(EXIT_FAILURE);
-	
+	if (nbytes <= 0)
+		exit(EXIT_FAILURE);
+
 	parentbuffer = malloc(sizeof(char) * nbytes);
 	if (parentbuffer == NULL) {
 		fprintf(stderr, "bad malloc");
@@ -186,14 +188,14 @@ main(int argc, char* argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	filenames = calloc(subfiles, sizeof(char*));
+	filenames = calloc(subfiles, sizeof(char *));
 	if (filenames == NULL) {
 		fprintf(stderr, "bad malloc");
 		free(parentbuffer);
 		exit(EXIT_FAILURE);
 	}
 
-	for(; j < subfiles; j++) {
+	for (; j < subfiles; j++) {
 		filenames[j] = malloc(len);
 		if (filenames[j] == NULL) {
 			fprintf(stderr, "bad malloc");
@@ -203,12 +205,13 @@ main(int argc, char* argv[])
 	}
 	createnames(filenames, subfiles, len, parentfile);
 
-	status = splitfile(filenames, subfiles, nbytes, parentfile, parentbuffer);
+	status =
+	    splitfile(filenames, subfiles, nbytes, parentfile, parentbuffer);
 	if (status == -1) {
-			fprintf(stderr, "usage: mysplit N file\n");
-			freeall(parentbuffer, filenames, subfiles);
-			exit(EXIT_FAILURE);
-		}
+		fprintf(stderr, "usage: mysplit N file\n");
+		freeall(parentbuffer, filenames, subfiles);
+		exit(EXIT_FAILURE);
+	}
 
 	freeall(parentbuffer, filenames, subfiles);
 	exit(EXIT_SUCCESS);
